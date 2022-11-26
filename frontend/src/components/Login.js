098ba69051c9c32e42a1b2cee6ddff2df032df31
DMN-SANGEETH <dmnsageeth@gmail.com>
2022-11-26 14:20:45 +0530

    aded front end

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,84 @@
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { useState } from 'react';
+// import styled from 'styled-components';
+import auth from './firebase/firebase';
+import "../styles/login.css";
+// import { BrowserRouter as Router,
+//         Routes,
+//         Route,
+//         Link,
+//  } from 'react-router-dom';
+
+
+
+const Login = () => {
+    const [username, setUsername] = useState(''); 
+    const [password, setPassword] = useState('');
+
+    const userSignin = ()=>{
+        signInWithEmailAndPassword(auth, username, password)
+        .then((userCredential) => {
+        // Signed in 
+        // const user = userCredential.user;
+        // // ...
+        // console.log(user);
+        console.log('User sign ind');
+        })
+        .catch((error) => {
+        //const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorMessage);
+        });
+}
+    // console.log(username);
+    // console.log(password);
+    return (
+        
+        <div className='container' >              
+        {/* <BlogImage>
+            <img src='./img/images02.jpeg' alt='blogimage' />
+        </BlogImage> */}
+            <div className="screen__content">
+            
+                <div className="content_1">
+                    <img src='./img/70bm.gif' alt= "logimag"/>
+                </div>
+
+                <div className='content_2'>
+                    <h2>User LogIn</h2>
+                    <label>Enter username  :</label><br/>
+                    <input className="login__input" type='text' placeholder='UserName' value={username} 
+                    onChange={e=>{
+                        e.preventDefault();
+                        setUsername(e.target.value);
+                    }}/> 
+                    <br/><br/>
+                    <label>Enter password :</label><br/>
+                    <input className="login__input" type='password' placeholder="password" value={password}
+                    onChange={e=>{
+                        e.preventDefault();
+                        setPassword(e.target.value);
+                    }}/>
+                    <br/><br/>
+
+                    <button onClick={userSignin} className="button login__submit">
+                        <div style={{align:'center',margin:'auto'}}>Log in</div>
+                    </button>
+                    <br/>
+                    {/* <li><Link to="/Patientsreg">React</Link></li> */}
+                    {/* <a href="/Patientsreg" className='patientsreg' >Sign-up</a> */}
+
+                    {/* <a a href='/' className='forgetpw' style={{textDecoration:'none',color:'ButtonHighlight'}}>forget password</a> */}
+                    
+                </div>
+            
+            </div>
+                   
+        </div>
+
+        
+        
+);
+}
+export default Login;
+
---
+++ frontend/src/components/firebase/firebase.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { getApp, initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+import { getAuth, createUserWithEmailAndPassword  } from "firebase/auth"; 
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyChttGsCdO0bjxbqnksLXYgYx6N8L7dzQY",
+  authDomain: "dnn-project-82042.firebaseapp.com",
+  projectId: "dnn-project-82042",
+  storageBucket: "dnn-project-82042.appspot.com",
+  messagingSenderId: "75766107776",
+  appId: "1:75766107776:web:8dc0d0ebb013ed2f6b6065",
+  measurementId: "G-4SVN96318B"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+//const app = !getApp().length? initializeApp(firebaseConfig): getApp(); 
+const auth = getAuth(app);
+
+export default auth;
+
+export function signup(email,password){
+  return createUserWithEmailAndPassword(auth, email,password);
+}
